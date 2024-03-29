353cf993d5cb131ec22f59ee35a4b6eb43c6f85f
tshakeel-debug <taaha2000@gmail.com>
2024-03-28 21:14:12 -0600

    Updated Assistance.js

--- client/src/Assistance/Assistance.js
+++ client/src/Assistance/Assistance.js
@@ -1,28 +1,27 @@
 import React, { useState, useEffect, useContext } from 'react';
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {Button, Typography, Container, Box} from '@mui/material'
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom'
-import GlobalStyles from '@mui/material/GlobalStyles';
-import Deluxe_Menu from "../images/Deluxe Menu.png";
+import { Button, Typography, Container, Box, CircularProgress } from '@mui/material';
+import { useParams } from 'react-router-dom';
 
-const Assistance= () => {
-
-  const {id} = useParams();  //this is either deluxe or regular
+const Assistance = () => {
+  const { id } = useParams();  // This is either deluxe or regular
   const [error, setError] = useState("");
 
-
-    return (
-      <>
-      ASSITANCE PAGE
-
-      
+  return (
+    <>
+      <Container maxWidth="sm" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+        <Box p={2} sx={{ border: '2px solid grey', bgcolor: 'white', textAlign: 'center', marginBottom: '10px'}}>
+          <Typography variant="h4" gutterBottom>
+            Assistance on the way.
+            <br />
+            Please wait...
+          </Typography>
+        </Box>
+        
+        <Box sx={{ position: 'relative', marginTop: '190px', width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start' }}>
+          <CircularProgress sx={{ alignSelf: 'center', marginBottom: '20px' }} />
+          <Button variant="contained" color="error" sx={{ marginTop: '190px' }}>Cancel</Button>
+        </Box>
+      </Container>
     </>
   );
 };
