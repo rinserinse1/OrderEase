a4fd7c01fa5e77d9b9c6293f40e2ce2bab1b7ba5
Winston Thai <winstonthai1@gmail.com>
2024-03-30 13:30:21 -0600

    Updated menu select page

--- client/src/MenuRelated/MenuSelect.js
+++ client/src/MenuRelated/MenuSelect.js
@@ -1,113 +1,64 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {Button, Typography, Container} from '@mui/material'
+import React from 'react';
+import { Box, ButtonBase, Card, CardMedia, Container, Grid, Typography, Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
-import Grid from '@mui/material/Grid';
-import { Card, CardMedia,Box, ButtonBase } from '@mui/material';
 
-import Deluxe_Menu from "../images/Deluxe Menu.png";
-import Regular_Menu from "../images/Regular Menu.png";
-import GlobalStyles from '@mui/material/GlobalStyles';
-import Stack from '@mui/material/Stack';
+import DeluxeMenuImage from "../images/Deluxe Menu.png";
+import RegularMenuImage from "../images/Regular Menu.png";
+
+const menus = [
+  { name: 'Deluxe Menu', image: DeluxeMenuImage, link: '/menuselectconfirm/deluxe' },
+  { name: 'Regular Menu', image: RegularMenuImage, link: '/menuselectconfirm/regular' }
+];
+
+const MenuOption = ({ name, image, link }) => (
+  <Grid item xs={12} sm={6}>
+    <Box display="flex" justifyContent="center">
+      <Card sx={{ maxWidth: '100%', boxShadow: 3, border: 1 }}> 
+        <ButtonBase component={Link} to={link} sx={{ width: '100%' }}>
+          <Stack direction="column">
+            <CardMedia
+              component="img"
+              sx={{ height: { xs: 200, sm: 250, md: 300 } }} 
+              image={image}
+              alt={name}
+            />
+            <Box sx={{ backgroundColor: 'white', textAlign: 'center', p: '10px' }}>
+              <Typography variant="h6" fontWeight='bold'>
+                {name}
+              </Typography>
+            </Box>
+          </Stack>
+        </ButtonBase>
+      </Card>
+    </Box>
+  </Grid>
+);
 
 const MenuSelectConfirm = () => {
-
-  const [error, setError] = useState("");
-
   return (
     <>
-
-      <br/><br/><br/><br/>
-      <Container maxWidth="sm">
-      <Box
-  height="100%"
-  display="flex"
-  alignItems="center"
-  justifyContent="center"
-  flexDirection="column"
-  p={2}
-  sx={{ border: '2px solid grey', bgcolor: "white", boxShadow: 2 }}
->
-  <Typography variant="h4" align="center" gutterBottom fontWeight='medium'>
-    Welcome to <br/> OrderEase! <br/> Pick your menu:
-  </Typography>
-</Box>
-      </Container>
-      <br/><br/><br/><br/>
+      <Box sx={{ my: 8 }}>
+        <Container maxWidth="sm">
+          <Box
+            height="100%"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+            p={2}
+            sx={{ border: '2px solid grey', bgcolor: "white", boxShadow: 2 }}
+          >
+            <Typography variant="h4" align="center" gutterBottom fontWeight='medium'>
+              Welcome to <br/> OrderEase! <br/> Pick your menu:
+            </Typography>
+          </Box>
+        </Container>
+      </Box>
 
       <Grid container justifyContent="center" spacing={2}>
-        <Grid item xs={12} sm={6}>
-        <Box display="flex" justifyContent="center">
-
-          <Card sx={{ maxWidth: 345, boxShadow: 3, border: 1}}>
-            <ButtonBase
-              component={Link}
-              to="/menuselectconfirm/deluxe"
-              style={{ width: '100%'}}
-            >
-              <Stack direction="column">
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={Deluxe_Menu}
-                  alt="Deluxe Menu"
-                />
-                <Box
-                sx={{
-                  backgroundColor: 'white',
-                  textAlign: 'center',
-                  padding: '10px',
-                }}
-                >
-                  <Typography variant="h6" component="div" fontWeight='bold'>
-                    Deluxe Menu
-                  </Typography>
-                </Box>
-              </Stack>
-            </ButtonBase>
-          </Card>
-          </Box>
-
-        </Grid>
-
-        <Grid item xs={12} sm={6}>
-        <Box display="flex" justifyContent="center">
-
-          <Card sx={{ maxWidth: 345, marginTop: 10, boxShadow: 3, border: 1 }}>
-            <ButtonBase
-              component={Link}
-              to="/menuselectconfirm/regular"
-              style={{ width: '100%' }}
-            >
-              <Stack direction="column">
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={Regular_Menu}
-                />
-                <Box
-                sx={{
-                  backgroundColor: 'white',
-                  textAlign: 'center',
-                  padding: '10px',
-                }}
-                >
-                  <Typography variant="h6" component="div" fontWeight='bold'>
-                    Regular Menu
-                  </Typography>
-                </Box>
-              </Stack>
-            </ButtonBase>
-          </Card>
-          </Box>
-
-        </Grid>
+        {menus.map(menu => (
+          <MenuOption key={menu.name} {...menu} />
+        ))}
       </Grid>
     </>
   );
