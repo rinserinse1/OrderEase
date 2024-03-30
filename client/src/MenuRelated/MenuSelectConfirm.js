3ffd4214e72c2742f0481f81d93e0374c8e0c5ea
Winston Thai <winstonthai1@gmail.com>
2024-03-30 14:36:42 -0600

    Menu select confirm adjustments

--- client/src/MenuRelated/MenuSelectConfirm.js
+++ client/src/MenuRelated/MenuSelectConfirm.js
@@ -1,16 +1,9 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useState } from 'react';
 import {Button, Typography, Container, Box} from '@mui/material'
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom'
-import GlobalStyles from '@mui/material/GlobalStyles';
-import Deluxe_Menu from "../images/Deluxe Menu.png";
+import Deluxe_Menu from "../images/Deluxe_Menu_Final.PNG";
+import Regular_Menu from "../images/Regular_Menu_Final.png"
 
 const MenuSelectConfirm = () => {
 
@@ -45,17 +38,17 @@ const MenuSelectConfirm = () => {
         mt={2} // Adjust margin top as needed
       >
         <img
-          src={Deluxe_Menu}
+          src={id === "deluxe" ? Deluxe_Menu : Regular_Menu}
           alt="Deluxe Menu"
           style={{
             maxHeight: '100%',
             maxWidth: '100%',
-            width: 'auto',
-            height: 'auto'
+            width: '92vw',
+            height: 'auto',
           }}
         />
       </Box>
-<br></br>
+      <br></br>
       <Container maxWidth="sm">
         <Box
           height="100%"
@@ -67,17 +60,26 @@ const MenuSelectConfirm = () => {
           sx={{ border: '2px solid grey', bgcolor: "white" }}
         >
           <Typography variant="h6" align="center" gutterBottom>
-              {id === 'deluxe' && (
-            "DELUXE DESC"
-          )}
-            {id === 'regular' && (
-          "REG DESC"
+            {id === 'deluxe' ? (
+              <>
+                Upgrade to the Deluxe Menu for an all-inclusive Korean BBQ adventure. Beyond the Regular Menu's favorites, you'll get exclusive access to 'Deluxe Delights', including:
+                <Box component="ul" sx={{ m: 0, p: 0, listStyleType: 'none' }}>
+                  <li><b>Marinated Beef Short Ribs:</b><br />Grilled to perfection.</li>
+                  <li><b>Savory Pork Dumplings:</b><br />Crispy and flavorful.</li>
+                  <li><b>Creamy Corn Cheese:</b><br />A cheesy delight.</li>
+                  <li><b>Cold Buckwheat Noodles:</b><br />Refreshing and light.</li>
+                  <li><b>Pork Backbone Stew:</b><br />Rich and hearty.</li>
+                  <li><b>Sizzling Dolsot Bibimbap:</b><br />A hot stone pot of mixed rice and vegetables.</li>
+                </Box>
+                It's the ultimate choice for those eager to explore the full spectrum of Korean culinary wonders.
+              </>
+            ) : (
+              "Discover the core of Korean BBQ with our Regular Menu, featuring classic favorites like Bulgogi, Spicy Rice Cakes, and a selection of traditional sides and desserts. It's the perfect choice for those looking to enjoy timeless Korean dishes in a comfortable and familiar setting."
             )}
-    </Typography>
+          </Typography>
         </Box>
       </Container>
-
-    <br></br>
+      <br/><br/>
       <Container maxWidth="sm">
         <Box
           height="100%"
@@ -107,10 +109,6 @@ const MenuSelectConfirm = () => {
           </Box>
         </Box>
       </Container>
-
-      <Typography variant="h6" align="center" gutterBottom>
-        Menu Select = {id}
-      </Typography>
 
       <br/><br/>
 
