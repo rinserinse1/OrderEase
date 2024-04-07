b474a2bf9f6ea370e04a38e63e806327671b1ec2
niran.malla <niran.malla@ucalgary.ca>
2024-04-06 22:43:11 -0600

    menu select and Menu select confirm, fonts hav been changed and fixed. the prices have also been added. fixed alignment issue

--- client/src/MenuRelated/MenuSelect.js
+++ client/src/MenuRelated/MenuSelect.js
@@ -1,32 +1,72 @@
-import React from 'react';
-import { Box, ButtonBase, Card, CardMedia, Container, Grid, Typography, Stack } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {
+  Box,
+  ButtonBase,
+  Card,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+  Stack,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
 import DeluxeMenuImage from "../images/Deluxe_Menu_Final.PNG";
 import RegularMenuImage from "../images/Regular_Menu_Final.png";
 
 const menus = [
-  { name: 'Deluxe Menu', image: DeluxeMenuImage, link: '/menuselectconfirm/deluxe', price: '$39.99' },
-  { name: 'Regular Menu', image: RegularMenuImage, link: '/menuselectconfirm/regular', price: '$29.99' }
+  {
+    name: "Deluxe Menu",
+    image: DeluxeMenuImage,
+    link: "/menuselectconfirm/deluxe",
+    price: "$39.99",
+  },
+  {
+    name: "Regular Menu",
+    image: RegularMenuImage,
+    link: "/menuselectconfirm/regular",
+    price: "$29.99",
+  },
 ];
 
 const MenuOption = ({ name, image, link, price }) => (
   <Grid item xs={12} sm={6}>
     <Box display="flex" justifyContent="center">
-      <Card sx={{ maxWidth: '100%', boxShadow: 3, border: 1 }}> 
-        <ButtonBase component={Link} to={link} sx={{ width: '100%' }}>
+      <Card
+        sx={{
+          maxWidth: "100%",
+          boxShadow: 3,
+          border: 1,
+          fontFamily: "Roboto Mono",
+        }}
+      >
+        <ButtonBase component={Link} to={link} sx={{ width: "100%" }}>
           <Stack direction="column">
             <CardMedia
               component="img"
-              sx={{ height: { xs: 200, sm: 250, md: 300 }, objectFit: "cover", width: "60vw" }} 
+              sx={{
+                height: { xs: 200, sm: 250, md: 300 },
+                objectFit: "cover",
+                width: "60vw",
+              }}
               image={image}
               alt={name}
             />
-            <Box sx={{ backgroundColor: 'white', textAlign: 'center', p: '10px' }}>
-              <Typography variant="h6" fontWeight='bold'>
+            <Box
+              sx={{ backgroundColor: "white", textAlign: "center", p: "10px" }}
+            >
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                sx={{ fontFamily: "Roboto Mono" }}
+              >
                 {name}
               </Typography>
-              <Typography variant="subtitle1" fontWeight='bold'>
+              <Typography
+                variant="subtitle1"
+                fontWeight="bold"
+                sx={{ fontFamily: "Roboto Mono" }}
+              >
                 {price}
               </Typography>
             </Box>
@@ -41,7 +81,7 @@ const MenuSelectConfirm = () => {
   localStorage.clear();
   return (
     <>
-      <Box sx={{ my: 4 }}>
+      <Box sx={{ my: 4, fontFamily: "Roboto Mono" }}>
         <Container maxWidth="sm">
           <Box
             height="100%"
@@ -50,24 +90,46 @@ const MenuSelectConfirm = () => {
             justifyContent="center"
             flexDirection="column"
             p={2}
-            sx={{ border: '2px solid grey', bgcolor: "white", boxShadow: 2, mb: -2, borderRadius: '10px' }}
+            sx={{
+              border: "2px solid grey",
+              bgcolor: "white",
+              boxShadow: 2,
+              mb: -2,
+              borderRadius: "10px",
+            }}
+          >
+            <Typography
+              variant="h5"
+              align="center"
+              gutterBottom
+              fontWeight="Bold"
+              sx={{ fontFamily: "Roboto Mono" }}
             >
-            <Typography variant="h5" align="center" gutterBottom fontWeight='Bold'>
-              Thank you for choosing OrderEase to enhance your dining experience!
+              Thank you for choosing OrderEase to enhance your dining
+              experience!
             </Typography>
-            <Typography variant="h6" align="center" mt={1} gutterBottom fontWeight='Medium'>
-              Welcome to AYCE KBBQ, please pick one of our two all you can eat menus below.
+            <Typography
+              variant="h6"
+              align="center"
+              mt={1}
+              gutterBottom
+              fontWeight="Light"
+              sx={{ fontFamily: "Roboto Mono" }}
+            >
+              Welcome to All You Can Eat KBBQ, please pick one of our two all
+              you can eat menus below.
             </Typography>
           </Box>
         </Container>
       </Box>
 
       <Grid container justifyContent="center" spacing={3}>
-        {menus.map(menu => (
+        {menus.map((menu) => (
           <MenuOption key={menu.name} {...menu} />
         ))}
       </Grid>
-      <br/><br/>
+      <br />
+      <br />
     </>
   );
 };
--- client/src/MenuRelated/MenuSelectConfirm.js
+++ client/src/MenuRelated/MenuSelectConfirm.js
@@ -1,20 +1,23 @@
-import React, { useState } from 'react';
-import {Button, Typography, Container, Box} from '@mui/material'
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom'
+import React, { useState } from "react";
+import { Button, Typography, Container, Box } from "@mui/material";
+import { Link, useParams } from "react-router-dom";
 import Deluxe_Menu from "../images/Deluxe_Menu_Final.PNG";
-import Regular_Menu from "../images/Regular_Menu_Final.png"
+import Regular_Menu from "../images/Regular_Menu_Final.png";
 
 const MenuSelectConfirm = () => {
-
-  const {id} = useParams(); 
+  const { id } = useParams();
   const [error, setError] = useState("");
 
+  const menuPrices = {
+    deluxe: "$39.99",
+    regular: "$29.99",
+  };
 
-    return (
-      <>
-    <br></br><br></br><br></br>
-
+  return (
+    <>
+      <br />
+      <br />
+      <br />
       <Container maxWidth="sm">
         <Box
           height="100%"
@@ -23,33 +26,38 @@ const MenuSelectConfirm = () => {
           justifyContent="center"
           flexDirection="column"
           p={2}
-          sx={{ border: '2px solid grey', bgcolor: "white", borderRadius: '10px', font: 'Roboto Mono' }}
+          sx={{
+            border: "2px solid grey",
+            bgcolor: "white",
+            borderRadius: "10px",
+            font: "Roboto Mono",
+          }}
         >
-          <Typography variant="h4" align="center" gutterBottom>
+          <Typography
+            variant="h4"
+            align="center"
+            gutterBottom
+            style={{ fontFamily: "Roboto Mono", fontWeight: "bold" }}
+          >
             You have selected the {id} menu!
           </Typography>
         </Box>
       </Container>
 
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        mt={2} 
-      >
+      <Box display="flex" justifyContent="center" alignItems="center" mt={2}>
         <img
           src={id === "deluxe" ? Deluxe_Menu : Regular_Menu}
-          alt="Deluxe Menu"
+          alt={`${id} Menu`}
           style={{
-            maxHeight: '100%',
-            maxWidth: '100%',
-            width: '92vw',
-            height: 'auto',
-            borderRadius: '10px'
+            maxHeight: "100%",
+            maxWidth: "100%",
+            width: "92vw",
+            height: "auto",
+            borderRadius: "10px",
           }}
         />
       </Box>
-      <br></br>
+      <br />
       <Container maxWidth="sm">
         <Box
           height="100%"
@@ -58,21 +66,83 @@ const MenuSelectConfirm = () => {
           justifyContent="center"
           flexDirection="column"
           p={2}
-          sx={{ border: '2px solid grey', bgcolor: "white", borderRadius: '10px', fontFamily: 'Roboto Mono' }}
+          sx={{
+            border: "2px solid grey",
+            bgcolor: "white",
+            borderRadius: "10px",
+            fontFamily: "Roboto Mono",
+          }}
         >
-          <Typography variant="h6" align="center" gutterBottom>
-            {id === 'deluxe' ? (
+          <Typography
+            variant="h6"
+            align="center"
+            gutterBottom
+            style={{ fontFamily: "Roboto Mono" }}
+          >
+            {id === "deluxe" ? (
               <>
-                Upgrade to the Deluxe Menu for an all-inclusive Korean BBQ adventure. Beyond the Regular Menu's favorites, you'll get exclusive access to 'Deluxe Delights', including:
-                <Box component="ul" sx={{ m: 0, p: 0, listStyleType: 'none' }}>
-                  <li><b> 1. Marinated Beef Short Ribs:</b><br />Grilled to perfection.</li>
-                  <li><b> 2. Savory Pork Dumplings:</b><br />Crispy and flavorful.</li>
-                  <li><b> 3. Creamy Corn Cheese:</b><br />A cheesy delight.</li>
-                  <li><b> 4. Cold Buckwheat Noodles:</b><br />Refreshing and light.</li>
-                  <li><b> 5. Pork Backbone Stew:</b><br />Rich and hearty.</li>
-                  <li><b> 6. Sizzling Dolsot Bibimbap:</b><br />A hot stone pot of mixed rice and vegetables.</li>
+                Upgrade to the Deluxe Menu for an all-inclusive Korean BBQ
+                adventure. Beyond the Regular Menu's favorites, you'll get
+                exclusive access to 'Deluxe Delights', including:
+                <Box
+                  component="ul"
+                  sx={{
+                    m: 0,
+                    p: 0,
+                    listStyleType: "none",
+                    fontFamily: "Roboto Mono",
+                  }}
+                >
+                  {[
+                    {
+                      title: "1. Marinated Beef Short Ribs:",
+                      description: "Grilled to perfection.",
+                    },
+                    {
+                      title: "2. Savory Pork Dumplings:",
+                      description: "Crispy and flavorful.",
+                    },
+                    {
+                      title: "3. Creamy Corn Cheese:",
+                      description: "A cheesy delight.",
+                    },
+                    {
+                      title: "4. Cold Buckwheat Noodles:",
+                      description: "Refreshing and light.",
+                    },
+                    {
+                      title: "5. Pork Backbone Stew:",
+                      description: "Rich and hearty.",
+                    },
+                    {
+                      title: "6. Sizzling Dolsot Bibimbap:",
+                      description:
+                        "A hot stone pot of mixed rice and vegetables.",
+                    },
+                  ].map((item, index) => (
+                    <li key={index}>
+                      <Typography
+                        variant="body1"
+                        component="div"
+                        sx={{ fontWeight: "bold", fontFamily: "Roboto Mono" }}
+                      >
+                        {item.title}
+                      </Typography>
+                      <Typography
+                        variant="body1"
+                        sx={{
+                          ml: 2,
+                          fontFamily: "Roboto Mono",
+                          fontStyle: "italic",
+                        }}
+                      >
+                        {item.description}
+                      </Typography>
+                    </li>
+                  ))}
                 </Box>
-                It's the ultimate choice for those eager to explore the full spectrum of Korean culinary wonders.
+                It's the ultimate choice for those eager to explore the full
+                spectrum of Korean culinary wonders.
               </>
             ) : (
               "Discover the core of Korean BBQ with our Regular Menu, featuring classic favorites like Bulgogi, Spicy Rice Cakes, and a selection of traditional sides and desserts. It's the perfect choice for those looking to enjoy timeless Korean dishes in a comfortable and familiar setting."
@@ -80,7 +150,8 @@ const MenuSelectConfirm = () => {
           </Typography>
         </Box>
       </Container>
-      <br/><br/>
+      <br />
+      <br />
       <Container maxWidth="sm">
         <Box
           height="100%"
@@ -89,35 +160,60 @@ const MenuSelectConfirm = () => {
           justifyContent="center"
           flexDirection="column"
           p={2}
-          sx={{ border: '2px solid grey', bgcolor: "white", borderRadius: '10px', fontFamily: 'Roboto Mono' }}
+          sx={{
+            border: "2px solid grey",
+            bgcolor: "white",
+            borderRadius: "10px",
+            fontFamily: "Roboto Mono",
+          }}
         >
-        
-          <Typography variant="h4" align="center" gutterBottom>
-           Confirm menu selection
+          <Typography
+            variant="h4"
+            align="center"
+            gutterBottom
+            style={{ fontFamily: "Roboto Mono", fontWeight: "bold" }}
+          >
+            Confirm menu selection
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            gutterBottom
+            style={{ fontFamily: "Roboto Mono", fontWeight: "bold" }}
+          >
+            Price: {menuPrices[id]}
           </Typography>
 
-          <Box display="flex" justifyContent="space-between" alignItems='center' mt={2} sx={{width: '100%'}}>
-
-          <Button variant="contained" color="error" component={Link}  to="/menuselect" sx={{ marginLeft: 2, marginRight: 2, width: '100%' }}>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mt={2}
+            sx={{ width: "100%" }}
+          >
+            <Button
+              variant="contained"
+              color="error"
+              component={Link}
+              to="/menuselect"
+              sx={{ marginLeft: 2, marginRight: 2, width: "100%" }}
+            >
               Cancel
             </Button>
-
-            <Button variant="contained"  color="success" component={Link} to={`/menucategories/${id}`} sx={{ marginRight: 2, width: '100%' }}>
+            <Button
+              variant="contained"
+              color="success"
+              component={Link}
+              to={`/menucategories/${id}`}
+              sx={{ marginRight: 2, width: "100%" }}
+            >
               Confirm
             </Button>
-
-           
           </Box>
         </Box>
       </Container>
-
-      <br/><br/>
-
-     
-
-      <br/><br/>
-
-      
+      <br />
+      <br />
     </>
   );
 };
